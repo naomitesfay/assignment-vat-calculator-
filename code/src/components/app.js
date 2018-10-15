4c64923b316d2b31e81f977e3b44b4039c3db95f
Naomi <naomi.tesfay@gmail.com>
2018-10-15 20:49:26 +0200

    assignment done

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -6,77 +6,98 @@ class App extends React.Component {
 constructor(props) {
   super(props)
   this.state = {
-    value: 'katt',
-    vatRate: 25,
-    incVat: 0,
-    exVat: 0
+  vatRate: 25,
+  incVat: 0,
+  exVat: 0
   }
 
 }
 
-handleincVat = (event) => {
-    this.setState ({
-      value: event.target.value
+handleRadioChange = (event) => {
+  const vat = parseInt(event.target.value, 10)
+  this.setState ({
+  vatRate: vat,
+  incVat: exVatToIncVat(vat, this.state.exVat)
+
     })
   }
 
-handleExtVat = (event) => {
+handleIncVat = (event) => {
+  const inc = parseInt(event.target.value, 10)
   this.setState ({
-    selectedValue: event.target.value
+  incVat: inc,
+  exVat: incVatToExtVat(this.state.vatRate, inc)
+
+    })
+  }
+
+handleExVat = (event) => {
+  const ex = parseInt(event.target.value, 10)
+  this.setState ({
+      exVat: ex,
+      incVat: exVatToIncVat(this.state.vatRate, ex)
   })
 
 }
-
-
-handlevatRate = (event) => {
-  this.setState ({
-    selectedValue: event.target.value
-  })
-
-}
-
-
 
   render() {
     return (
       <div className="App">
-        <div className="radiocontainer">
-
-      <p>Example calculating ex vat for 1000kr inc vat @ 25%: {incVatToExtVat(25, 1000)}</p>
-      <p>Example calculating inc vat for 600kr ex vat @ 6%: {exVatToIncVat(6, 600)}</p>
-
           <div className="Momscontainer">
             <form>
+              <label htmlFor="25%">25%</label>
+                  <input type="radio"
+                  value="25"
+                  checked={this.state.vatRate === 25}
+                  onChange={this.handleRadioChange}
+                  />
+
+
+              <label htmlFor="12%">12%</label>
+                  <input type="radio"
+                    value="12"
+                    checked={this.state.vatRate === 12}
+                    onChange={this.handleRadioChange}
+                    />
+
+
+              <label htmlFor="6%">6%</label>
+                  <input type="radio"
+                    value="6"
+                    checked={this.state.vatRate === 6}
+                    onChange={this.handleRadioChange}
+                    />
+
+
               <label>
                   Inklusive moms (kr):
-                    <input type="text"
-                    name="name"
-                    placeholder="0"
-                    value={this.state.value}
-                    onChange={this.handleIncVat}
-                    />
-              </label>
-
-          <input type="submit" value="Submit" />
-              <label>
-                Exklusive moms (kr):
-                  <input type="text"
-                  name="name"
+                  <input type="number"
                   placeholder="0"
+                  value={this.state.incVat}
+                  onChange={this.handleIncVat}
                   />
               </label>
 
-          <input type="submit" value="Submit" />
-            <label>
-            Momssumma (kr):
-                  <input type="text" name="name" placeholder="0"/>
+
+              <label>
+                  Exklusive moms (kr):
+                  <input type="number"
+                  placeholder="0"
+                  value={this.state.exVat}
+                  onChange={this.handleExVat}
+                  />
               </label>
-                  <input type="submit" value="Submit" />
-
-        </form>
 
 
-          </div>
+
+              <label>
+                  Momssumma (kr):
+                  <input type="number"
+                  name="name"
+                  value= {this.state.incVat - this.state.exVat} />
+              </label>
+
+          </form>
         </div>
       </div>
     )
